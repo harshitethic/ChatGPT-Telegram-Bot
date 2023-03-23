78b64508d33fd2a4ba881090f2adf364c7f30a88
Harshit Sharma <66710144+harshitethic@users.noreply.github.com>
2023-03-23 06:34:04 +0530

    Update harshitethic.py

--- harshitethic.py
+++ harshitethic.py
@@ -20,8 +20,6 @@ else:
     tgkey = tgenv
 print(tgenv)
 
-
-
 # Lots of console output
 debug = True
 
@@ -296,29 +294,4 @@ def interact(bot, update, botname, username, new):
 
 def error(bot, update):
     """Log Errors caused by Updates."""
-    logger.warning('Update "%s" caused error "%s"', update)
-
-
-def main():
-    """Start the bot."""
-
-    updater = Updater(tgkey, use_context=False)
-    # Get the dispatcher to register handlers
-    dp = updater.dispatcher
-    # on different commands - answer in Telegram
-    dp.add_handler(CommandHandler("start", start))
-    dp.add_handler(CommandHandler("help", help))
-    dp.add_handler(CommandHandler("reset", reset))
-    dp.add_handler(CommandHandler("retry", retry))
-    # on noncommand i.e message - echo the message on Telegram
-    dp.add_handler(MessageHandler(Filters.text, runn))
-    # log all errors
-    dp.add_error_handler(error)
-    # Start the Bot
-    updater.start_polling()
-   
-    updater.idle()
-
-
-if __name__ == '__main__':
-    main()
+    logger.warning('Update "%s" caused error "%s
